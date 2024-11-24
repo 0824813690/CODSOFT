1bd0d072bf230f47d5c2a5da870ce9b89a6b2de5
Lerato <hlehlelerato8@gmail.com>
2024-11-24 04:29:51 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,98 +1,37 @@
-// Tic-Tac-Toe game: TASK 3.
+
+ //TASK ONE: NUMBER GUESSING GAME
+
 #include <iostream>
-#include <vector>
-using namespace std;
-
-void displayBoard(const vector<vector<char>>& board) {
-    cout << "\n";
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            cout << board[i][j];
-            if (j < 2) cout << " | ";
-        }
-        cout << "\n";
-        if (i < 2) cout << "--+---+--\n";
-    }
-    cout << "\n";
-}
-
-
-bool checkWin(const vector<vector<char>>& board, char player) {11
-
-    for (int i = 0; i < 3; i++) {
-        if ((board[i][0] == player && board[i][1] == player && board[i][2] == player) ||
-            (board[0][i] == player && board[1][i] == player && board[2][i] == player)) {
-            return true;
-        }
-    }
-    if ((board[0][0] == player && board[1][1] == player && board[2][2] == player) ||
-        (board[0][2] == player && board[1][1] == player && board[2][0] == player)) {
-        return true;
-    }
-    return false;
-}
-
-
-bool checkDraw(const vector<vector<char>>& board) {
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            if (board[i][j] == ' ') {
-                return false;
-            }
-        }
-    }
-    return true;
-}
-
-
-void playGame() {
-    vector<vector<char>> board(3, vector<char>(3, ' '));
-    char currentPlayer = 'X';
-    bool gameRunning = true;
-
-    while (gameRunning) {
-        displayBoard(board);
-
-
-        int row, col;
-        cout << "Player " << currentPlayer << ", enter your move (row and column, 1-3): ";
-        cin >> row >> col;
-        row--; col--;
-
-
-        if (row < 0 || row >= 3 || col < 0 || col >= 3 || board[row][col] != ' ') {
-            cout << "Invalid move. Try again.\n";
-            continue;
-        }
-
-
-        board[row][col] = currentPlayer;
-
-
-        if (checkWin(board, currentPlayer)) {
-            displayBoard(board);
-            cout << "Player " << currentPlayer << " wins!\n";
-            gameRunning = false;
-        } else if (checkDraw(board)) {
-            displayBoard(board);
-            cout << "It's a draw!\n";
-            gameRunning = false;
-        } else {
-
-            currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
-        }
-    }
-}
-
-
+#include <cstdlib>
+#include <ctime>
+ using namespace std;
 int main() {
-    char playAgain;
-    do {
-        playGame();
-        cout << "Do you want to play again? (y/n): ";
-        cin >> playAgain;
-    } while (playAgain == 'y' || playAgain == 'Y');
 
-    cout << "Thanks for playing!\n";
+    srand(time(0));
+
+    int randomNumber = rand() % 100 + 1;
+    int guess = 0;
+
+    cout << "Welcome to the Number Guessing Game!" << endl;
+    cout << "I have selected a random number between 1 and 100." << endl;
+    cout << "can you guess it? then you can START!" << endl;
+    cout << "\n";
+    cout << "*****GOODLUCK*****"<< endl;
+   while (true) {
+        cout << "Enter your guess:    ";
+        cin >> guess;
+
+        if (guess > randomNumber) {
+            cout << "\nToo high! Try again.\n\n";
+        } else if (guess < randomNumber) {
+            cout << "\nToo low! Try again.\n\n";
+        } else {
+            cout << "\nCongratulations! You guessed the correct number: "
+                      << randomNumber << "\n\n";
+            break;
+        }
+    }
+
+
     return 0;
 }
